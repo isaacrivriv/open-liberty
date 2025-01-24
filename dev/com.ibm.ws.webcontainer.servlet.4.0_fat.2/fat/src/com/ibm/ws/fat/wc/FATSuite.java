dc3c99f7b76b5ad609bdeced43d427da49117bd9
Paul Nicolucci <pnicolucci@gmail.com>
2025-01-24 13:29:32 -0500

    Netty CTS test debugging

--- dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
+++ dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
@@ -15,6 +15,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.ibm.ws.fat.util.FatLogHandler;
+import com.ibm.ws.fat.wc.tests.NettyTests;
 import com.ibm.ws.fat.wc.tests.WCPartitionedAttributeTests;
 import com.ibm.ws.fat.wc.tests.WCPartitionedCookieAttributeSecurityTest;
 import com.ibm.ws.fat.wc.tests.WCRequestAutoDecompressTest;
@@ -50,17 +51,17 @@ import com.ibm.ws.fat.wc.tests.NettyTests;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-                //WCRequestAutoDecompressTest.class,
-                //WCResponseContentLengthChunkedEncodingTest.class,
-                //WCResponseHeadersTest.class,
-                //WCSameSiteCookieAttributeTests.class,
-                //WCSameSiteCookieAttributeSecurityTest.class,
-                //WCPartitionedAttributeTests.class,
-                //WCPartitionedCookieAttributeSecurityTest.class,
-                //WCSameSiteIncompatibleClientsTests.class,
-                //WebAndWebFragmentServletMappingTests.class,
-                //WebSphereServletEventListenerTest.class,
-                //WebSphereSpiHttpRequestURLTest.class,
+                WCRequestAutoDecompressTest.class,
+                WCResponseContentLengthChunkedEncodingTest.class,
+                WCResponseHeadersTest.class,
+                WCSameSiteCookieAttributeTests.class,
+                WCSameSiteCookieAttributeSecurityTest.class,
+                WCPartitionedAttributeTests.class,
+                WCPartitionedCookieAttributeSecurityTest.class,
+                WCSameSiteIncompatibleClientsTests.class,
+                WebAndWebFragmentServletMappingTests.class,
+                WebSphereServletEventListenerTest.class,
+                WebSphereSpiHttpRequestURLTest.class,
                 NettyTests.class
 })
 
