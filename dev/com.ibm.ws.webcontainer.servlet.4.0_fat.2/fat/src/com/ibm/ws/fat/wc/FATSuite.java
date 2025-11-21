5925d1397c1323035e52ece0d128ef1920f4f37c
Paul Nicolucci <pnicolucci@gmail.com>
2025-11-21 10:25:02 -0500

    Only run nioInputTest

--- dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
+++ dev/com.ibm.ws.webcontainer.servlet.4.0_fat.2/fat/src/com/ibm/ws/fat/wc/FATSuite.java
@@ -10,7 +10,6 @@
 package com.ibm.ws.fat.wc;
 
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -31,6 +30,7 @@ import com.ibm.ws.fat.wc.tests.WebSphereSpiHttpRequestURLTest;
 import componenttest.rules.repeater.EmptyAction;
 import componenttest.rules.repeater.FeatureReplacementAction;
 import componenttest.rules.repeater.RepeatTests;
+import com.ibm.ws.fat.wc.tests.NettyTests;
 
 /**
  * Servlet 4.0 Tests
@@ -50,17 +50,18 @@ import componenttest.rules.repeater.RepeatTests;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-                WCRequestAutoDecompressTest.class,
-                WCResponseContentLengthChunkedEncodingTest.class,
-                WCResponseHeadersTest.class,
-                WCSameSiteCookieAttributeTests.class,
-                WCSameSiteCookieAttributeSecurityTest.class,
-                WCPartitionedAttributeTests.class,
-                WCPartitionedCookieAttributeSecurityTest.class,
-                WCSameSiteIncompatibleClientsTests.class,
-                WebAndWebFragmentServletMappingTests.class,
-                WebSphereServletEventListenerTest.class,
-                WebSphereSpiHttpRequestURLTest.class
+                //WCRequestAutoDecompressTest.class,
+                //WCResponseContentLengthChunkedEncodingTest.class,
+                //WCResponseHeadersTest.class,
+                //WCSameSiteCookieAttributeTests.class,
+                //WCSameSiteCookieAttributeSecurityTest.class,
+                //WCPartitionedAttributeTests.class,
+                //WCPartitionedCookieAttributeSecurityTest.class,
+                //WCSameSiteIncompatibleClientsTests.class,
+                //WebAndWebFragmentServletMappingTests.class,
+                //WebSphereServletEventListenerTest.class,
+                //WebSphereSpiHttpRequestURLTest.class,
+                NettyTests.class
 })
 
 public class FATSuite {
@@ -69,11 +70,13 @@ public class FATSuite {
     // EE11 requires Java 17
     // If we only specify EE10/EE11 for lite mode it will cause no tests to run with lower Java versions which causes an error.
     // If we are running with a Java version less than 11, have EE9 be the lite mode test to run.
-    @ClassRule
-    public static RepeatTests repeat = RepeatTests.with(new EmptyAction().fullFATOnly())
-                    .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
-                    .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
-                    .andWith(FeatureReplacementAction.EE11_FEATURES());
+    /*
+     * @ClassRule
+     * public static RepeatTests repeat = RepeatTests.with(new EmptyAction().fullFATOnly())
+     * .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+     * .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+     * .andWith(FeatureReplacementAction.EE11_FEATURES());
+     */
 
     /**
      * @see {@link FatLogHandler#generateHelpFile()}
